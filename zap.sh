a523f6952f1a5e6d6ed93565ba7c82169e9882dc
Hamzabenrhou <hamza26benrhouma02@gmail.com>
2025-07-09 22:47:30 +0100

    ensure zap is working

--- zap.sh
+++ zap.sh
@@ -1,13 +1,15 @@
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
-chmod 777 $(pwd)
+mkdir -p $(pwd)/zap/wrk
+chmod 755 $(pwd)/zap/wrk
 echo "$(id -u):$(id -g)"
-docker run -v $(pwd)/zap/wrk/:/rw -t zaproxy/zap-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+docker run -v $(pwd)/zap/wrk/:/zap/wrk -t zaproxy/zap-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
 
 sudo mkdir -p owasp-zap-report
-sudo mv zap_report.html owasp-zap-report
+sudo mv $(pwd)/zap/wrk/zap_report.html owasp-zap-report
+sudo chown $(id -u):$(id -g) owasp-zap-report
 
 echo "Exit Code : $exit_code"
 
