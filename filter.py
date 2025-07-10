bec18be7653e90ed94669351d31185c7235f2887
Hamzabenrhou <hamza26benrhouma02@gmail.com>
2025-07-10 13:48:06 +0100

    trying

--- filter.py
+++
@@ -1,5 +0,0 @@
-def alertFilter(alert):
-    ignore_ids = [10023, 90022]
-    if int(alert.getPluginId()) in ignore_ids:
-        return False  # Ignore this alert
-    return True  # Keep all others
