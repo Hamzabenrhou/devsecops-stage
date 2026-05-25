861adf1cf6d557c3ecd33a7e0b5da3ecbf632aa4
Hamzabenrhou <hamza26benrhouma02@gmail.com>
2026-05-25 11:44:38 +0100

    com

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -66,5 +66,6 @@ public class NumericController {
         logger.info("Value Received in Request - " + value);
         logger.info("Node Service Response - " + response);
         return Integer.parseInt(response);
+        
     }
 }
